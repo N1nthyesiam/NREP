7bd940368ebf979757c7304d42cc5cb865140807
N!nth <117446593+N1nthyesiam@users.noreply.github.com>
2022-11-11 09:37:26 +1100

    Delete NREP directory

--- NREP/utils/logpong.py
+++
@@ -1,9 +0,0 @@
-class Debug:
-	def log(*data, end="\n"):
-		print("[INFO]", *data, end=end)
-
-	def warn(*data, end="\n"):
-		print("[WARN]", *data, end=end)
-
-	def err(*data, end="\n"):
-		print("[ERROR]", *data, end=end)

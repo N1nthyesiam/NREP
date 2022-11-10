bab10e23ccb641b79b95e17508cff88a5c323fd4
N!nth <117446593+N1nthyesiam@users.noreply.github.com>
2022-11-11 09:31:23 +1100

    Add files via upload

--- Examples/speed_test.py
+++ Examples/speed_test.py
@@ -1,33 +1,46 @@
-import requests, time
+import requests, time, os
 
 proxy = {
     "http": "http://localhost:8080",
     "https": "http://localhost:8080"
     }
 
-tests_count = 20
+tests_count = 10
+t1,t2,t3,t4=[],[],[],[]
+
+for _ in range(tests_count):
+    t = os.popen("curl -o /dev/null --proxy http://localhost:8080 -s https://www.google.com/ -w %{time_total}\n").read()
+    t1.append(float(t))
 
 sess = requests.Session()
-
-t1 = time.time()
 sess.get("https://www.google.com/", proxies=proxy)
-t1 = time.time()-t1
+for _ in range(tests_count):
+    t = time.perf_counter()
+    sess.get("https://www.google.com", proxies=proxy)
+    t = time.perf_counter()-t
+    t2.append(t)
 
-t2 = time.time()
-sess.get("https://www.google.com", proxies=proxy)
-t2 = time.time()-t2
+for _ in range(tests_count):
+    t = os.popen("curl -o /dev/null -s https://www.google.com/ -w %{time_total}\n").read()
+    t3.append(float(t))
 
 sess = requests.Session()
-
-t3 = time.time()
 sess.get("https://www.google.com")
-t3 = time.time()-t3
+for _ in range(tests_count):
+    t = time.perf_counter()
+    sess.get("https://www.google.com")
+    t = time.perf_counter()-t
+    t4.append(t)
 
-t4 = time.time()
-sess.get("https://www.google.com")
-t4 = time.time()-t4
-
-print(f"With NREP (includes handshake): {t1} s")
-print(f"With NREP (no handshake): {t2} s")
-print(f"Without NREP (includes handshake): {t3} s")
-print(f"Without NREP (no handshake): {t4} s")
+t1 = sum(t1)/len(t1)
+t2 = sum(t2)/len(t2)
+t3 = sum(t3)/len(t3)
+t4 = sum(t4)/len(t4)
+p1 = round(t1/t3*100-100,1)
+p2 = round(t2/t4*100-100,1)
+print(f"With NREP (no pipeing): {t1}s")
+print(f"With NREP (pipeing): {t2}s")
+print(f"Without NREP (first request): {t3}s")
+print(f"Without NREP (second request): {t4}s")
+print(f"NREP is {abs(p1)} % {'slower' if p1<=0 else 'faster'}")
+print(f"NREP (pipeing) is {abs(p2)} % {'slower' if p2<=0 else 'faster'}")

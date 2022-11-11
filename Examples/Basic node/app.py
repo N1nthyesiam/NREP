542869a033bdf9fb05eef0ab9a0768982b289125
N!nth <117446593+N1nthyesiam@users.noreply.github.com>
2022-11-11 13:23:41 +1100

    Add files via upload

--- Examples/Basic node/app.py
+++ Examples/Basic node/app.py
@@ -1,7 +1,5 @@
 from NREP.node import Node
 from NREP.utils.spec import Config
-from NREP.bridge.http2nrep import Bridge
 
 config = Config('config.json')
 node = Node(config)
-# Bridge("127.0.0.1", 8080)
